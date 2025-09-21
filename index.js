6aa47c9da5c9331ca07bd0c9540ea9193cf82cb7
Tomonobu Fujioka <fujioka.tomonob@fujitsu.com>
2025-09-21 22:18:19 +0900

    mod

--- index.js
+++ index.js
@@ -198,19 +198,6 @@ var event = new function(){
 		$('#answer').show();
 		$('#next').hide();
 		
-		// カービゴール
-		if(何問目 >= 問題数) {
-
-			clearInterval(dededeAnimeId);
-			
-			if(カービ移動回数 >= デデデ移動回数) {
-				event.win();
-			}else{
-				event.lose();
-			}
-			return;
-		}
-		
 		// カービィ移動
 		event.moveKirby();
 		
@@ -221,6 +208,19 @@ var event = new function(){
 			$('#answer').hide();
 			$('#next').show();
 			
+			// カービゴール
+			if(何問目 >= 問題数) {
+
+				clearInterval(dededeAnimeId);
+				
+				if(カービ移動回数 >= デデデ移動回数) {
+					event.win();
+				}else{
+					event.lose();
+				}
+				return;
+			}
+		
 			event.takeMondai();
 			
 		}, 500);
